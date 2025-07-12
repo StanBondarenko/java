98ebbf558d7e0c7ea95ea763bbf0de2b3960eed1
Stanislav Bondarenko <s.stan.bond@gmail.com>
2025-07-12 18:21:59 -0400

    Delete Class Math directory

--- Class Math/src/Run.java
+++
@@ -1,8 +0,0 @@
-import java.math.BigInteger;
-
-public class Run {
-    public static void main(String[] arg) {
-        System.out.println(MyMath.myQuadEquation(3, -14, -5));
-        System.out.println(MyMath.myRaiseToPower(3,-10));
-    }
-}
