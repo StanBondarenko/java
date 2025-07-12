7531295348967d4d39387039c7bc5a67a416f80e
Stanislav Bondarenko <s.stan.bond@gmail.com>
2025-07-12 18:21:13 -0400

    Add files via upload

---
+++ Java Company Structure/src/Department.java
@@ -0,0 +1,22 @@
+public class Department {
+    private int departmentId;
+    private String name;
+
+    public void setDepartmentId(int departmentId){
+        this.departmentId= departmentId;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public int getDepartmentId(){
+        return this.departmentId;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public Department(int departmentId, String name){
+        this.departmentId=departmentId;
+        this.name=name;
+    }
+
+}
